b9c84c7f4c17c87d300c4f80c52f76aa19b7699e
Guy Arad <gmorphus@hotmail.com>
2014-11-06 15:52:44 +0200

    Fixing target compare tools folder
    In Tower 2's recent version, custom compare tools shoud be under
    CompareTools folder

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
-cp CompareTools.plist ~/Library/Application\ Support/com.fournova.Tower2/
-cp -R CompareScripts/* ~/Library/Application\ Support/com.fournova.Tower2/
+mkdir ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
+cp CompareTools.plist ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
+cp -R CompareScripts/* ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
